bba689ed7487eac593cacdf484d01e47059a8408
Hina-Gauhar <hinagauhar34@gmail.com>
2026-05-08 14:52:35 +0500

    Create problem_01.py

---
+++ branching/problem_01.py
@@ -0,0 +1,11 @@
+# find the minimum of 3 numbers
+a=int(input("enter a number : "))
+b=int(input("enter a number : "))
+c=int(input("enter a number : "))
+
+if(a<b and a<c):
+    print(a, " is the smallest")
+elif(b<a and b<c):
+    print(b, " is the smallest")
+else:
+    print(c," is the smallest")
