515792001098ed866f159990f6ba0f70141b7e56
Hina-Gauhar <hinagauhar34@gmail.com>
2026-05-09 09:47:00 +0500

    Create problem_01.py

---
+++ lists/problem_01.py
@@ -0,0 +1,11 @@
+# write a python program to merge 2 lists without + operator
+l1=[1,2,3,4,5]
+l2=[6,7,8,9,10]
+
+l3=[]
+for i in l1:
+    l3.append(i)
+for i in l2:
+    l3.append(i)
+    
+print(l3)
